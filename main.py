149209060b999d08e4ef854edd1b2678be8dc8ce
olusegun <olusegunbamgbelu755@gmail.com>
2024-02-09 23:20:11 +0100

    learnt for loops

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 #variable 
 
 name = "olusegun"
+
+#for loop
+
+numbers = [1, 2, 4, 6 ,7 ,8 , 9, 20, 3, 5, 12, 14, 95, 86, 72]
+
+for num in numbers:
+    if num % 2 == 0:
+        continue
+    print("This is an odd number", num)
+print("Game is over")
