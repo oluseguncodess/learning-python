0b6019687a33d41713e033af76f93cc2cb673a07
olusegun <olusegunbamgbelu755@gmail.com>
2024-02-12 23:22:00 +0100

    Do exercises
    Operations and assignment operators in python

--- main.py
+++ main.py
@@ -24,3 +24,41 @@ for num in numbers:
         continue
 #     print("This is an odd number", num)
 # print("Game is over")
+
+
+
+
+
+
+
+
+
+
+
+
+#exercises 
+
+#Exercise 1
+
+#Suppose the cost of an item as $600. After a month, the cost is doubled due to exchange rates. About 3
+# weeks later, the cost is reduced by $150. After a week, the cost is increased by $100.
+# What is the new cost of the item? Write in python code.
+
+initial_cost = 600
+
+#Ater a month, intital cost doubled
+
+initial_cost *= 2
+
+# after 3 weeks, cost is reduced by $150
+
+initial_cost -= 150
+
+#After a week, cost has an increment of $100
+
+initial_cost += 100
+
+print("The new cost of the item is: $" + str(initial_cost))
+
+
+
