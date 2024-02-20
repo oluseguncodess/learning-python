e94b6465321c6834fc2d4e330999449c6f2e2efe
olusegun <olusegunbamgbelu755@gmail.com>
2024-02-20 23:45:41 +0100

    update documentation

--- doc.py
+++ doc.py
@@ -4,3 +4,10 @@
 
 # This is used to store values of different data types 
 name_of_variable = value
+
+# Functions
+# These are reuseable blocks of code used to perform a task. 
+
+# e.g the print function
+print("I am a cow")
+# this function is the most used as it's used to display information in a console
